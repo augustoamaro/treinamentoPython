225f60648c0da950b08d25f9925646c0c52b42b5
Augusto Amaro <augustoamaro@icloud.com>
2017-06-12 19:29:59 -0300

    alteracoes

---
+++ Iniciante/1008.py
@@ -0,0 +1,8 @@
+numero = int(input())
+horaTrabalhada = int(input())
+valorHora = float(input())
+
+salario = float(horaTrabalhada * valorHora)
+
+print("NUMBER = %d" %numero)
+print("SALARY = U$ %0.2f" %salario)
